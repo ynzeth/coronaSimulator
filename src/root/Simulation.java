4dcafe5e183a5e259fd36b9d36673b6458deb852
Ynze <ynzbook@outlook.com>
2020-04-01 00:19:39 +0200

    Add simulation class

--- src/root/Simulation.java
+++ src/root/Simulation.java
@@ -1,7 +1,51 @@
 package root;
 
 public class Simulation {
-    public Simulation() {
+    //init variables
+    private int amountOfPeople;
+    private double radius;
+    private double chance;
 
+    //simulation variables
+    private int[][] locations; // two-dimentional array containing locations and speed for all particles
+    private boolean[] infections; // array containing information on whether particles have been infected
+
+    public Simulation(int amountOfPeople, double radius, double chance) {
+        this.amountOfPeople = amountOfPeople;
+        this.radius = radius;
+        this.chance = chance;
+
+        startSimulation();
+    }
+
+    public void updateAmountOfPeople(int amountOfPeople) {
+        this.amountOfPeople = amountOfPeople;
+        startSimulation();
+    }
+
+    public void updateRadius(double radius) {
+        this.radius = radius;
+        startSimulation();
+    }
+
+    public void updateChance(double chance) {
+        this.chance = chance;
+        startSimulation();
+    }
+
+    private void startSimulation(){
+        locations = new int[this.amountOfPeople][3];
+
+        for(int i = 0; i < this.amountOfPeople; i++){
+            locations[i][0] = (int) (Math.random() * 100);
+            locations[i][1] = (int) (Math.random() * 100);
+            locations[i][2] = (int) (Math.random() * 5);
+        }
+
+        this.infections = new boolean[this.amountOfPeople];
+
+        for(int i = 0; i < this.amountOfPeople; i++){
+            infections[i] = false;
+        }
     }
 }
--- src/root/Window.java
+++ src/root/Window.java
@@ -8,7 +8,7 @@ import javafx.scene.text.Text;
 
 public class Window {
     private Slider radiusSlider;
-    private Slider speedSlider;
+    private Slider chanceSlider;
     private Slider populationSlider;
 
     private Group g;
@@ -20,37 +20,43 @@ public class Window {
         this.g = new Group();
         this.s = new Scene(g, 800,  800);
 
-        this.sim = new Simulation();
+        this.sim = new Simulation(5, 10, 0.1);
         initializeScrollers();
     }
 
     private void initializeScrollers() {
         this.radiusSlider = new Slider();
         this.populationSlider = new Slider();
-        this.speedSlider = new Slider();
+        this.chanceSlider = new Slider();
 
         radiusSlider.setMin(1);
         radiusSlider.setMax(10);
+        radiusSlider.setValue(5);
         populationSlider.setMin(2);
         populationSlider.setMax(50);
-        speedSlider.setMin(1);
-        speedSlider.setMax(20);
+        populationSlider.setValue(10);
+        chanceSlider.setMin(0);
+        chanceSlider.setMax(1);
+        chanceSlider.setValue(0.1);
 
         radiusSlider.valueProperty().addListener((obs, oldVal, newVal) -> {
             System.out.println("Radius: " + newVal);
+            this.sim.updateRadius((double) newVal);
         });
         populationSlider.valueProperty().addListener((obs, oldVal, newVal) -> {
             System.out.println("Population: " + newVal);
+            this.sim.updateAmountOfPeople(newVal.intValue());
         });
-        speedSlider.valueProperty().addListener((obs, oldVal, newVal) -> {
-            System.out.println("Speed: " + newVal);
+        chanceSlider.valueProperty().addListener((obs, oldVal, newVal) -> {
+            System.out.println("Chance of infection: " + newVal);
+            this.sim.updateChance((double) newVal);
         });
 
         Text radiusText = new Text(" Radius: ");
         Text populationText = new Text(" Population: ");
         Text speedText = new Text(" Speed: ");
 
-        g.getChildren().add(new HBox(radiusText, radiusSlider, populationText, populationSlider, speedText, speedSlider));
+        g.getChildren().add(new HBox(radiusText, radiusSlider, populationText, populationSlider, speedText, chanceSlider));
     }
 
     public Scene getScene() {
